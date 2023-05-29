941afd78da86e549cd8a028608c14a06787f4bcf
Ednan <52186220+ednanf@users.noreply.github.com>
2023-05-29 10:30:35 -0300

    Initial Commit

---
+++ Project6b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Ednan R. Frizzera Filho on 29/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
